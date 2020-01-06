2bde03ef4ea9c3c8dcb4b980a6080e5479a48508
yevver <verhovich@gmail.com>
2020-01-06 18:57:23 +0200

    Lesson_13_2

--- src/main/java/ua/verkhovych/springcourse/Computer.java
+++ src/main/java/ua/verkhovych/springcourse/Computer.java
@@ -3,12 +3,11 @@ package ua.verkhovych.springcourse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Computer {
     private int id;
     private MusicPlayer musicPlayer;
 
-    @Autowired
     public Computer(MusicPlayer musicPlayer) {
         id = 1;
         this.musicPlayer = musicPlayer;
